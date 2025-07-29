4e6d8a558416a9a989541420bd7cefa91f9fb8c1
Luiz Togni <contato.togni@gmail.com>
2025-07-29 16:23:20 -0300

    test: add pet deleter controller tests

---
+++ src/controllers/pet_deleter_controller_test.py
@@ -0,0 +1,8 @@
+from .pet_deleter_controller import PetDeleterController
+
+def test_delete(mocker):
+    mock_repository = mocker.Mock()
+    controller = PetDeleterController(mock_repository)
+    controller.delete("petName")
+
+    mock_repository.delete_pets.assert_called_once_with("petName")
