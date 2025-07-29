ce1836237f7866a6491f5e67207c86e3dfc72250
Luiz Togni <contato.togni@gmail.com>
2025-07-29 16:09:06 -0300

    feat: add pet lister controller

---
+++ src/controllers/pet_lister_controller.py
@@ -0,0 +1,30 @@
+from typing import List, Dict
+from src.models.sqlite.entities.pets import PetsTable
+from src.models.sqlite.interfaces.pets_repository import PetsRepositoryInterface
+
+class PetListerController:
+    def __init__(self, pets_repository: PetsRepositoryInterface) -> None:
+        self.__pets_repository = pets_repository
+
+    def list(self) -> Dict:
+        pets = self.__get_pets_in_db()
+        formatted_response = self.__format_response(pets)
+        return formatted_response
+
+    def __get_pets_in_db(self) -> List[PetsTable]:
+        pets = self.__pets_repository.list_pets()
+        return pets
+
+    def __format_response(self, pets: List[PetsTable]) -> Dict:
+        formatted_pets = []
+
+        for pet in pets:
+            formatted_pets.append({ "name": pet.name, "type": pet.type, "id": pet.id })
+
+        return {
+            "data": {
+                "type": "Pets",
+                "count": len(formatted_pets),
+                "attributes": formatted_pets
+            }
+        }
