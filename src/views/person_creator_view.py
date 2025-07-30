08f746cd7bd679938e8776ef77dfc5d4a6482464
Luiz Togni <contato.togni@gmail.com>
2025-07-30 20:47:36 -0300

    refactor: add person creator validator

--- src/views/person_creator_view.py
+++ src/views/person_creator_view.py
@@ -1,6 +1,7 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 from src.controllers.interfaces.person_creator_controller import PersonCreatorControllerInterface
+from src.main.validators.person_creator_validator import person_creator_validator
 from .interfaces.view_interface import ViewInterface
 
 class PersonCreatorView(ViewInterface):
@@ -8,6 +9,8 @@ class PersonCreatorView(ViewInterface):
         self.__controller = controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
+        person_creator_validator(http_request)
+
         person_info = http_request.body
         body_response = self.__controller.create(person_info)
 
