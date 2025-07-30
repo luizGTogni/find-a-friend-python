ad9d3684c2cee7e39a4d0053dfc3eb562b4cad22
Luiz Togni <contato.togni@gmail.com>
2025-07-30 16:52:52 -0300

    refactor: change Exception error to Custom errors

--- src/controllers/person_finder_controller.py
+++ src/controllers/person_finder_controller.py
@@ -1,6 +1,7 @@
 from typing import Dict
 from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
 from src.models.sqlite.entities.people import PeopleTable
+from src.main.error.error_types.http_not_found import HttpNotFoundError
 from .interfaces.person_finder_controller import PersonFinderControllerInterface
 
 class PersonFinderController(PersonFinderControllerInterface):
@@ -15,7 +16,7 @@ class PersonFinderController(PersonFinderControllerInterface):
     def __find_person_in_db(self, person_id: int) -> PeopleTable:
         person = self.__people_repository.get_person(person_id)
         if not person:
-            raise Exception("Person not found")
+            raise HttpNotFoundError("Person not found")
 
         return person
 
