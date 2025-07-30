dcd1f04bfdb005c7265f63386979249e77f0486f
Luiz Togni <contato.togni@gmail.com>
2025-07-30 16:31:45 -0300

    feat: add delete pet route

--- src/main/routes/pets_routes.py
+++ src/main/routes/pets_routes.py
@@ -1,11 +1,20 @@
 from flask import Blueprint, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.main.composer.pet_lister_composer import pet_lister_composer
+from src.main.composer.pet_deleter_composer import pet_deleter_composer
 
 pet_route_bp = Blueprint("pets_routes", __name__)
 
 @pet_route_bp.route("/pets", methods=["GET"])
 def list_pets():
+    http_request = HttpRequest()
     view = pet_lister_composer()
-    response = view.handle(HttpRequest())
-    return jsonify(response.body), response.body
+    http_response = view.handle(http_request)
+    return jsonify(http_response.body), http_response.status_code
+
+@pet_route_bp.route("/pets/<name>", methods=["DELETE"])
+def delete_pet(name):
+    http_request = HttpRequest(param={ "name": name })
+    view = pet_deleter_composer()
+    http_response = view.handle(http_request)
+    return jsonify(http_response.body), http_response.status_code
